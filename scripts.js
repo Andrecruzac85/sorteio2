c5dadbcdd6cd5aca62a6a7693be0c40f70838eaf
Andrecruzac85 <bomber.andre@gmail.com>
2025-06-01 20:25:01 -0300

    resultado no h2

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 function resultGenerator(){
 const min = Math.ceil ( document.querySelector(".input-min").value);
 const max = Math.floor (document.querySelector(".input-max").value);
+const resultado = document.querySelector(".resultadoSorteio")
+if(min < max){
 const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-
-alert(result)
+resultado.innerHTML = result
 }
+else{
+     resultado.innerHTML="o primeiro numero devera ser menor que o segundo"
+}
+}
+
+
